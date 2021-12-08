b97d4798811938dbf0d7d638cada801acca5231c
Jardel Lima <jardel.lima@indexdigital.com.br>
2021-12-07 21:00:12 -0300

    Estudando Hooks
    - Estudando useEffect.

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -2,28 +2,37 @@ import React from "react";
 import Produto from "./Produto";
 
 const App = () => {
-    const [dados, setDados] = React.useState(null);
-    const [carregando, setCarregando] = React.useState(null);
+    const [produto, setProduto] = React.useState(null);
 
-    async function handleClick(event) { 
-        setCarregando(true);
-        const response = await fetch(
-          `https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`,
-        );
-
-        const json = await response.json();
-        setDados(json);
-        setCarregando(false);
+    function handleClick({ target }) {
+        setProduto(target.innerText);
     }
+
+    React.useEffect(() => {
+        const produtoLocal = window.localStorage.getItem("Produto");
+
+        if(produtoLocal !== null ) {
+            setProduto(produtoLocal);
+        }
+
+    }, []);
+
+    React.useEffect(() => {
+
+        if(produto !== null) {
+            window.localStorage.setItem("Produto", produto)
+        }
+
+    }, [produto]);
 
     return (
         <>
-            <button style={{ margin: ".5rem" }} onClick={handleClick}>notebook</button>
-            <button style={{ margin: ".5rem" }} onClick={handleClick}>smartphone</button>
-            <button style={{ margin: ".5rem" }} onClick={handleClick}>tablet</button>
+            <h1>Sua preferencia: { produto }</h1>
 
-            {carregando && <p>Carregando...</p>}
-            {!carregando && dados && <Produto dados={dados} />} 
+            <button style={{marginRight: "1rem"}} onClick={handleClick}>notebook</button>
+            <button onClick={handleClick}>smartphone</button>
+
+            <Produto produto = { produto } />
         </>
     );
 }
