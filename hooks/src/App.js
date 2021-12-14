8fdb2079727de032992237b9652893681ff47392
Jardel Lima <jardel.lima@indexdigital.com.br>
2021-12-14 14:46:56 -0300

    Estudando Custom Hooks
    - Estudando Custom Hooks.

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,18 +1,38 @@
 import React from "react";
-import { GlobalStorage } from "./GlobalContext";
-import Limpar from "./Limpar";
-import Produto from "./Produto";
+import useFetch from "./useFetch";
+import useLocalStorage from "./useLocalStorage";
 
 const App = () => {
+    const [produto, setProduto] = useLocalStorage('produto', '');
+    const { request, data, loading, error } = useFetch();
 
-    return (
-        <>
-            <GlobalStorage>
-                <Produto />
-                <Limpar />
-            </GlobalStorage>
-        </>
-    );
+    React.useEffect(() => {
+        async function fetchData() {
+            const { response, json } = await request('https://ranekapi.origamid.dev/json/api/produto/notebook');
+        } 
+        
+        fetchData();
+    }, [request]);
+
+    function handleClick({ target }) {
+        setProduto(target.textContent);
+    }
+
+    if(error) return <p>{error}</p>
+    if(loading) return <p>Carregando...</p>
+    if(data) {
+        return (
+            <>
+                <p>Produto preferido: { produto }</p>
+                <button onClick={ handleClick }>Notebook</button>
+                <button onClick={ handleClick }>Smartphone</button>
+    
+                <div>{ data.nome }</div>
+            </>
+        );
+    } else {
+        return null;
+    }
 }
 
 export default App;
