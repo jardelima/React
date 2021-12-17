270c51f3a0a33ba60799a816c37c8432cb38f918
Jardel Lima <jardel.lima@indexdigital.com.br>
2021-12-17 12:16:00 -0300

    Exercício de formulário
    - Exercício de formulário.

--- forms/src/App.js
+++ forms/src/App.js
@@ -1,34 +1,97 @@
 import React from "react";
 
-function App() {
-  const [nome, setNome] = React.useState("");
-  const [email, setEmail] = React.useState("");
+const formFields = [
+    {
+      id: 'nome',
+      label: 'Nome',
+      type: 'text'
+    },
+    {
+      id: 'email',
+      label: 'Email',
+      type: 'email'
+    },
+    {
+      id: 'senha',
+      label: 'Senha',
+      type: 'password'
+    },
+    {
+      id: 'cep',
+      label: 'Cep',
+      type: 'text'
+    },
+    {
+      id: 'rua',
+      label: 'Rua',
+      type: 'text'
+    },
+    {
+      id: 'numero',
+      label: 'Numero',
+      type: 'text'
+    },
+    {
+      id: 'bairro',
+      label: 'Bairro',
+      type: 'text'
+    },
+    {
+      id: 'cidade',
+      label: 'Cidade',
+      type: 'text'
+    },
+    {
+      id: 'estado',
+      label: 'Estado',
+      type: 'text'
+    },
+]
 
-  // const [form, setForm] = React.useState({
-  //     nome: "",
-  //     email: "",
-  // });
+function App() {
+  const [form, setForm] = React.useState(
+      formFields.reduce((acc, field) => {
+          return {
+              ...acc,
+              [field.id]: "",
+          } 
+      }, {}),
+  );
+
+  const [response, setResponse] = React.useState(null);
+  
+
+  function handleChange({target}) {
+      const {id, value} = target;
+
+      setForm({ ...form, [id]:value })
+  }
 
   function handleSubmit(event) {
       event.preventDefault();
+
+      fetch('https://ranekapi.origamid.dev/json/api/usuario', {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json",
+          },
+          body: JSON.stringify(form),
+      }).then((response) => setResponse(response));
   }
-
-  // function handleChange({target}) {
-  //     const {id, value} = target;
-
-  //     setForm({ ...form, [id]:value })
-  // }
 
   return (
     <>
       <form onSubmit={handleSubmit}>
-          <label htmlFor="nome">Nome</label>
-          <input type="text" id="nome" name="nome" value={nome} onChange={(event) => setNome(event.target.value)} />
-          
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" name="email" value={email} onChange={(event) => setEmail(event.target.value)} />
+        {formFields.map(({id, label, type}) => 
+            <div key={id}> 
+                <label htmlFor={id}>{label}</label>
+                <input type={type} id={id} name={id} value={form[id]} onChange={handleChange} />
+            </div>
+        )}
 
-          <button>Enviar</button>
+        {response && response.ok && <p>Formulario Enviado</p>}
+
+        <button>Enviar</button>
       </form>
     </>
   );
