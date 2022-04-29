f1e1e90b4b43a2305ad774b706a4ae3eec01e335
Jardel Lima <jardel.lima@indexdigital.com.br>
2022-04-29 15:34:19 -0300

    Desafio formulários

--- forms/src/App.js
+++ forms/src/App.js
@@ -1,41 +1,94 @@
 import React from "react";
+import Radio from "./Form/Radio";
 
-const coresArray = ['azul', 'roxo', 'laranja', 'verde', 'vermelho', 'cinza'];
+const perguntas = [
+    {
+      pergunta: 'Qual método é utilizado para criar componentes?',
+      options: [
+        'React.makeComponent()',
+        'React.createComponent()',
+        'React.createElement()',
+      ],
+      resposta: 'React.createElement()',
+      id: 'p1',
+    },
+    {
+      pergunta: 'Como importamos um componente externo?',
+      options: [
+        'import Component from "./Component"',
+        'require("./Component")',
+        'import "./Component"',
+      ],
+      resposta: 'import Component from "./Component"',
+      id: 'p2',
+    },
+    {
+      pergunta: 'Qual hook não é nativo?',
+      options: ['useEffect()', 'useFetch()', 'useCallback()'],
+      resposta: 'useFetch()',
+      id: 'p3',
+    },
+    {
+      pergunta: 'Qual palavra deve ser utilizada para criarmos um hook?',
+      options: ['set', 'get', 'use'],
+      resposta: 'use',
+      id: 'p4',
+    },
+  ];  
 
-function App() {
-  const [cores, setCores] = React.useState([]);
+const App = () => {
+    const [respostas, setRespostas] = React.useState({
+        p1: '',
+        p2: '',
+        p3: '',
+        p4: '',
+    });
 
-  function handleChange({target}) {
-      const {checked, value } = target;
+    const [slide, setSlide] = React.useState(0);
+    const [resultado, setResultado] = React.useState(null);
 
-      if(checked) {
-          setCores([...cores, target.value]);
-      } else {
-          setCores(cores.filter((cor) => cor !== value));
-      }
-  }
+    function handleChange({ target }) {
+        setRespostas({ ...respostas, [target.id]: target.value });
+    }
 
-  function handleChecked(cor) {
-      return cores.includes(cor);
-  }
+    function resultadoFinal() {
+        const corretas = perguntas.filter( 
+            ({ id, resposta }) => respostas[id] === resposta, 
+        );
 
-  return (
-    <>
-        <form>
-            {coresArray.map((cor) => (
-              <label key={cor} style={{textTransform: "capitalize"}}>
-                  <input 
-                      type="checkbox"
-                      value={cor}
-                      checked={handleChecked(cor)}
-                      onChange={handleChange}
-                  />
-                  {cor}
-              </label>
-            ))}
+        setResultado(`Você acertou: ${ corretas.length } de ${ perguntas.length }`);
+   }
+
+    function handleClick() {
+        if(slide < perguntas.length - 1) {
+            setSlide(slide + 1);
+        } else {
+            setSlide(slide + 1);
+
+            resultadoFinal();
+        }
+    }
+
+    return (
+        <form onSubmit = { (event) => event.preventDefault() }>
+            {perguntas.map((pergunta, index) => (
+                <Radio 
+                    active = { slide === index }
+                    key = { pergunta.id } 
+                    value = { respostas[pergunta.id] } 
+                    onChange = { handleChange } 
+                    { ...pergunta }
+                />
+            ))} 
+
+            {resultado ? (
+                <p>{ resultado }</p>
+            ) : (
+                <button onClick = { handleClick }>Próximo</button> 
+            )}
+            
         </form>
-    </>
-  );
+    )
 }
 
 export default App;
